eb386492ff3addda92bef10803cedbb3d0df23c3
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-08-12 09:58:38 -0700

    files

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require("dotenv").config();
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const multer = require("multer");
+const upload = multer({dest: "./uploads"});
+const cloudinary = require("cloudinary");
+const db = require("./models");
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+cloudinary.config(process.env.CLOUDINARY_URL);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(3000);
+app.post("/", upload.single("myFile"), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, (result) => {
+    db.cloudpic.findOrCreate({
+      where: {url: result.url}
+    }).then(() => {
+      res.redirect();
+    })
+  }).catch(error => {
+    console.log(error);
+  })
+});
+
+app.listen(4200);
